e4112a96be2cfbb991bf3e72e71722e843f5885c
Ishtmeet Singh <ish.rissam@gmail.com>
2022-01-23 19:31:55 +0530

    first working version with all the necessary details
    There are still a lot of changes needed to be made on this project. For example design changes, soem better visualization of the scores, and having a track of all the races completed. Showing author information etc...

--- src/App.js
+++ src/App.js
@@ -2,19 +2,93 @@ import TypingInput from "./components/TypingInput";
 import "./app.scss";
 import { useState } from "react";
 
-import quotes from './utils/quotes.json';
+import quotes from "./utils/quotes.json";
 
 function App() {
+  const [textToType, setTextToType] = useState(getRandomQuote());
+  const [currentWordIndex, setCurrentWordIndex] = useState(0);
+  const [currentCharacterIndex, setCurrentCharacterIndex] = useState(0);
+  const [currentTypedWord, setCurrentTypedWord] = useState("");
+  const [messages, setMessages] = useState([]);
 
-  const [textToType, setTextToType] = useState(quotes[Math.floor(Math.random() * quotes.length)])
+  function getRandomQuote() {
+    return quotes[Math.floor(Math.random() * quotes.length)];
+  }
+
+  function incrementWordIndex() {
+    setCurrentWordIndex(currentWordIndex + 1);
+  }
+
+  function updateCurrentWord(typedWord) {
+    setCurrentTypedWord(typedWord);
+  }
+
+  function refreshQuote() {
+    setTextToType(getRandomQuote());
+    setCurrentWordIndex(0);
+    setCurrentTypedWord('')
+    setMessages([])
+  }
+
+  function renderTextToType() {
+    return textToType.split(" ").map((word, wordIndex) => {
+      return (
+        <span
+          key={wordIndex}
+          className={`word ${currentWordIndex === wordIndex && "active"}`}
+        >
+          {word.split("").map((letter, letterIndex) => {
+            const isCorrect =
+              wordIndex < currentWordIndex ||
+              (currentWordIndex == wordIndex &&
+                currentTypedWord[letterIndex] === letter &&
+                letterIndex < currentTypedWord.length);
+
+            const notTypedYet =
+              wordIndex > currentWordIndex ||
+              currentTypedWord.length <= letterIndex;
+            return (
+              <span
+                key={letterIndex}
+                className={`letter ${
+                  isCorrect ? "correct" : notTypedYet ? "default" : "wrong"
+                }`}
+              >
+                {letter}
+              </span>
+            );
+          })}
+          {currentWordIndex == wordIndex &&
+            currentTypedWord.length > word.length && (
+              <span className="letter overwrite">
+                {currentTypedWord.substring(word.length)}
+              </span>
+            )}
+        </span>
+      );
+    });
+  }
+  console.log(currentTypedWord);
   return (
     <div className="container">
-      <h3 className="container__heading">Typeed - Check your typing speed</h3>
+      <h3 className="container__heading">Typeed</h3>
+      <h4>Check your typing speed!</h4>
       <div className="container__progress-box">
-        <p>{textToType}</p>
+        <p>{renderTextToType()}</p>
       </div>
       <div className="container__typing-area">
-        <TypingInput textToType={textToType} />
+        <TypingInput
+          updateCurrentWord={updateCurrentWord}
+          incrementWordIndex={incrementWordIndex}
+          textToType={textToType}
+          setMessages={setMessages}
+        />
+        <span className="button" onClick={refreshQuote}>Next</span>
+      </div>
+      <div className="result">
+        {messages.map((curr, index) => (
+          <p key={index}>{curr}</p>
+        ))}
       </div>
     </div>
   );
--- src/components/TypingInput.js
+++ src/components/TypingInput.js
@@ -1,51 +1,100 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./typing-input.scss";
 
-export default function TypingInput({ textToType }) {
+export default function TypingInput({
+  textToType,
+  updateCurrentWord,
+  updateCurrentCharacter,
+  incrementWordIndex,
+  setMessages,
+}) {
   const inputRef = useRef();
   const [startTime, setStartTime] = useState(null);
-  const [messages, setMessages] = useState([])
+  const [inputText, setInputText] = useState("");
+  const [textChunks, setTextChunks] = useState([]);
+  const [wordIndex, setWordIndex] = useState(0);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
+  useEffect(() => {
+    setTextChunks(textToType.split(" "));
+    startTypingTest();
+  }, [textToType]);
 
   function handleChange(e) {
     if (!startTime) setStartTime(new Date().getTime());
+    const validatedWord_bounds = e.target.value.substring(0, 25).trim();
+    setInputText(validatedWord_bounds);
+    updateCurrentWord(validatedWord_bounds);
+    if (
+      wordIndex === textChunks.length - 1 &&
+      e.target.value.trim() == textChunks[wordIndex]
+    ) {
+      return handleFinished();
+    }
+  }
 
-    if (e.target.value === textToType) {
-      handleFinished()
+  function onKeyDown(e) {
+    let { code } = e;
+    if (code === "Space") {
+      if (textChunks[wordIndex] == inputText) {
+        setInputText("");
+        if (wordIndex < textChunks.length - 1) {
+          setWordIndex(wordIndex + 1);
+          incrementWordIndex();
+        }
+      }
     }
   }
 
   function handleFinished() {
     const totalWords = textToType.length / 5;
-    const timeTaken = (new Date().getTime() - startTime) / 1000
-    const typingSpeed = (totalWords / (timeTaken))
+    const timeTaken = (new Date().getTime() - startTime) / 1000;
+    const typingSpeed = totalWords / timeTaken;
 
     setMessages([
       `Typed ${totalWords} words (${textToType.length} characters)`,
       `Time taken: ${timeTaken} seconds`,
-      `Typing speed (Words per second): ${parseFloat(totalWords/timeTaken).toFixed(2)}`,
-      `Typing speed (Words per minute): ${Math.round((totalWords / timeTaken) * 60)}`,
-      `Typing speed (Characters per minute): ${Math.round((textToType.length / timeTaken) * 60)}`,
-      `Typing speed (Characters per second): ${Math.round(textToType.length / timeTaken)} `
-    ])
+      `Typing speed (Words per second): ${parseFloat(typingSpeed).toFixed(2)}`,
+      `Typing speed (Words per minute): ${Math.round(typingSpeed * 60)}`,
+      `Typing speed (Characters per minute): ${Math.round(
+        (textToType.length / timeTaken) * 60
+      )}`,
+      `Typing speed (Characters per second): ${Math.round(
+        textToType.length / timeTaken
+      )} `,
+    ]);
 
-    setStartTime(null)
+    endTypingTest();
+
+    setStartTime(null);
+  }
+
+  function startTypingTest() {
+    inputRef.current.classList.remove("disabled");
+    inputRef.current.focus();   
+    setStartTime(null);
+    setInputText("");
+    setWordIndex(0);
+  }
+  /** Add startTypingTest() when the user presses - New test */
+  function endTypingTest() {
+    inputRef.current.classList.add("disabled");
+    inputRef.current.blur();
   }
 
   return (
     <>
-    <input
-      className="input"
-      ref={inputRef}
-      placeholder="Type here..."
-      onChange={handleChange}
-    />
-    <br />
-    <br />
-    <br />
-    <br />
-
-    {messages.map((curr, index) => <p key={index}>{curr}</p>)}
+      <input
+        className="input"
+        ref={inputRef}
+        placeholder="Type here..."
+        onChange={handleChange}
+        onKeyDown={onKeyDown}
+        value={inputText}
+      />
     </>
   );
 }
-
